3f984dacf2a2c329226b5775d6ca6b58d46e3eef
williandeitosi <williandeitosi@gmail.com>
2024-08-23 20:30:41 -0300

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import buildApp from "./app";
 
 const server = buildApp()
 
-server.listen({port: 3333})
+server.listen({port: 3333, host: '0.0.0.0'})
 .then((address)=>{console.log(`Server is running: ${address}`)})
 .catch(err => {
   console.log(`Error start server: ${err}`)
