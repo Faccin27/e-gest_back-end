ff43a2aa440c00a02531c4d17e8a24cc0633d5f1
Faccin27 <gfaccin27@gmail.com>
2024-08-26 16:47:32 -0300

    fix: configure CORS for frontend fetch requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const fastify = require('fastify')({ logger: true });
+const cors = require('@fastify/cors');
 const router = require('./routes/router');
 
+// CORS
+fastify.register(cors, {
+  origin: 'http://localhost:8080', /* Isso faz com que sejam aceitas apenas requisições do nosso frontend*/
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+});
+
+// Registrar rotas
 fastify.register(router);
 
 module.exports = fastify;
