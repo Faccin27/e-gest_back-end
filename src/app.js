6214b672c3607472fa3c2d9383433533462f56af
Faccin27 <gfaccin27@gmail.com>
2024-08-30 12:57:55 -0300

    Fix: Login routes and CreatedAt var fixed on register controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const fastify = require('fastify')({ logger: true });
 const cors = require('@fastify/cors');
+const jwt = require('@fastify/jwt');
 const router = require('./routes/router');
 
 // CORS
@@ -7,6 +8,19 @@ fastify.register(cors, {
   origin: 'http://localhost:8080', /* Isso faz com que sejam aceitas apenas requisições do nosso frontend*/
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 });
+
+// JWT
+fastify.register(jwt, {
+  secret: 'supersecret'
+});
+
+fastify.decorate("authenticate", async function (request, reply) {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.send(err);
+  }
+})
 
 // Registrar rotas
 fastify.register(router);
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,6 +4,8 @@ async function userRoutes(fastify, options) {
   fastify.get('/', UserController.getAllUsers);
   fastify.get('/:id', UserController.getUserById);
   fastify.post('/', UserController.createUser);
+  fastify.post('/register', UserController.register);
+  fastify.post('/login', UserController.login);
   fastify.put('/:id', UserController.updateUser);
   fastify.delete('/:id', UserController.deleteUser);
 }
