01b7903ca7877e03ba0e30fce5a4c8771451130b
williandeitosi <williandeitosi@gmail.com>
2024-08-30 19:26:50 -0300

    fix: routes

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,13 +1,14 @@
 const UserController = require('../controllers/UserController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 async function userRoutes(fastify, options) {
-  fastify.get('/', UserController.getAllUsers);
-  fastify.get('/:id', UserController.getUserById);
-  fastify.post('/', UserController.createUser);
-  fastify.post('/register', UserController.register);
-  fastify.post('/login', UserController.login);
-  fastify.put('/:id', UserController.updateUser);
-  fastify.delete('/:id', UserController.deleteUser);
+  fastify.get('/', { preHandler: authMiddleware }, UserController.getAllUsers);
+  fastify.get('/:id', { preHandler: authMiddleware }, UserController.getUserById);
+  fastify.post('/', { preHandler: authMiddleware }, UserController.createUser);
+  fastify.post('/register', { preHandler: authMiddleware }, UserController.register);
+  fastify.post('/login', { preHandler: authMiddleware }, UserController.login);
+  fastify.put('/:id', { preHandler: authMiddleware }, UserController.updateUser);
+  fastify.delete('/:id', { preHandler: authMiddleware }, UserController.deleteUser);
 }
 
 module.exports = userRoutes;
